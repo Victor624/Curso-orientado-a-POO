8944e0ad5639e7baf0b0c130f05269e734bb85e5
Victor Vargas <victorvargas023@gmail.com>
2022-10-13 12:47:53 -0300

    tercer Subida

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,11 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Car car= new Car();
+        car.license="amq252";
+        car.driver="adres herrera";
+        car.passegenger= 4;
+        System.out.println("Car Licencia", car.license);
+
     }
 }
